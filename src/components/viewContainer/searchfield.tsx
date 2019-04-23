bb0e2ea62aa7fb42098b23e9ba12189cf3d9382a
Sari <sari.vogel.94@gmail.com>
2019-04-23 18:37:20 +0200

    Change form to use redirect on submit

--- src/components/viewContainer/searchfield.tsx
+++ src/components/viewContainer/searchfield.tsx
@@ -1,18 +1,19 @@
 import React, { CSSProperties } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Button from '../button';
 
 interface Props {}
 
 interface State {
-    value: string
+    value: string,
+    redirect: boolean
 }
 
 export default class SearchField extends React.Component<Props, State> {
     constructor(props) {
       
       super(props);
-      this.state = {value: ''};
+      this.state = {value: '', redirect: false};
   
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,16 +27,31 @@ export default class SearchField extends React.Component<Props, State> {
       alert('A name was submitted: ' + this.state.value);
       event.preventDefault();
     }
+    
+    setRedirect = () => {
+        this.setState({
+          redirect: true
+        })
+    }
+    
+    renderRedirect = () => {
+        if (this.state.redirect) {
+          return <Redirect to={this.state.value} />
+        }
+    }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <Link to={this.state.value}>Submit</Link>
-        </form>
+        <div>
+            {this.renderRedirect()}        
+            <form onSubmit={this.setRedirect}>
+            <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+        </div>
       );
     }
   }
